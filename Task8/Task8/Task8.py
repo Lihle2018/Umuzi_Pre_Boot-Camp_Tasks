5e2b495d710216c1e197e98f9d18215dbfaff396
Mqhayi Lihle <2018293130@ufs4life.ac.za>
2020-08-11 18:19:13 +0200

    Task 8

---
+++ Task8/Task8/Task8.py
@@ -0,0 +1,10 @@
+num =int(input("Enter a number: "))
+
+def Convert(number):
+    hours=number//60
+    min=number%60
+    print(str(hours)+" hours, "+str(min)+" minutes\n")
+print()
+Convert(num)
+
+    
